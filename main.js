5d817d6876197a7a650735986b9bb9aff400c6c6
unknown <you@example.com>
2024-11-16 01:36:51 +0700

    created

---
+++ main.js
@@ -0,0 +1,57 @@
+const arrayDisplay = document.querySelector('.array-display');
+const array = JSON.parse(arrayDisplay.textContent.slice(6));
+const textContent = arrayDisplay.textContent;
+
+let currentArray = array;
+
+function changeData(array) {
+  return `Array: [${array.join(', ')}]`;
+}
+
+const sortBtn = document.querySelector('.btn-sort');
+
+sortBtn.onclick = () => {
+  currentArray = currentArray.sort((a, b) => a - b);
+  
+  arrayDisplay.innerText = changeData(currentArray);
+}
+
+const reverseBtn = document.querySelector('.btn-reverse');
+
+reverseBtn.onclick = () => {
+  currentArray = currentArray.sort((a, b) => b - a);
+  
+  arrayDisplay.innerText = changeData(currentArray);
+}
+
+const pushBtn = document.querySelector('.btn-push');
+
+pushBtn.onclick = () => {
+  array.push(10);
+  currentArray = array;
+  arrayDisplay.innerText = changeData(currentArray);
+}
+
+const popBtn = document.querySelector('.btn-pop');
+
+popBtn.onclick = () => {
+  array.pop(10);
+  currentArray = array;
+  arrayDisplay.innerText = changeData(currentArray);
+}
+
+const shiftBtn = document.querySelector('.btn-shift');
+
+shiftBtn.onclick = () => {
+  array.shift(10);
+  currentArray = array;
+  arrayDisplay.innerText = changeData(currentArray);
+}
+
+const unshiftBtn = document.querySelector('.btn-unshift');
+
+unshiftBtn.onclick = () => {
+  array.unshift(0);
+  currentArray = array;
+  arrayDisplay.innerText = changeData(currentArray);
+}
